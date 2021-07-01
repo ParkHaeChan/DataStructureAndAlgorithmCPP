eaf449289ebf0d30a5565c2c5911928af4378cdb
ParkHaeChan <lucidsun92@gmail.com>
2021-07-01 17:12:21 +0900

    calc permutation by recursion
    to deeply understand how recursion works...

---
+++ Recursion/Permutation_Overlaping_elements.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+std::vector<std::string> overlaping_permutations;
+
+void overlaping_permutation(std::string& inputstr, int current_index)
+{
+	//탈출 조건: 현재 위치가 문자열 끝인 경우
+	if (current_index == inputstr.size() - 1)
+	{
+		//std::cout << inputstr << std::endl;
+		overlaping_permutations.emplace_back(inputstr);
+		return;
+	}
+
+	// 현재 문자로 시작하는 경우
+	// 현재 문자 위치 포함해서 정렬
+	// 예시 입력: aaa..bbb..ccc..dddd...
+	std::sort(inputstr.begin() + current_index, inputstr.end());
+
+	// a로 시작하는 모든 case를 여기서 처리
+	overlaping_permutation(inputstr, current_index + 1);
+	
+	// 완료 후 현재 문자 위치 재외하고 다시 정렬하여 다음 재귀 진입 준비
+	// 현상태 예시: a(ddd..ccc..bb..aaaa...) --> 정렬 후: a(aa..bbb..ccc..ddd...)
+	std::sort(inputstr.begin() + current_index + 1, inputstr.end());
+
+	// 현재 문자로 시작하지 않는 경우 처리하는 법: 각 자리 문자와 현재 문자 위치 바꿔서 출력
+	for (int i = current_index + 1; i < inputstr.size(); ++i)
+	{
+		//서로 같은 원소이면 바꿔도 겹치므로 넘어간다
+		if (inputstr[current_index] == inputstr[i])
+		{
+			continue;
+		}
+
+		// 현재 문자가 순서상 다음 문자보다 작으면 현재 문자 위치와 다음 문자 위치 교환
+		if (inputstr[current_index] < inputstr[i])
+		{
+			char temp = inputstr[current_index];
+			inputstr[current_index] = inputstr[i];
+			inputstr[i] = temp;
+		}
+		
+		//bxxx, cxxx, dxxx로 시작하는 모든 case를 처리
+		overlaping_permutation(inputstr, current_index + 1);
+		// 현재 문자 위치 제외하고 다시 정렬(xxx부분)하여 다음 재귀 진입
+		std::sort(inputstr.begin() + current_index + 1, inputstr.end());
+	}
+}
+
+void print_permutations()
+{
+	for (auto e : overlaping_permutations)
+		std::cout << e << std::endl;
+}
+
+int main()
+{
+	std::string inputstring = "cabc";
+
+	overlaping_permutation(inputstring, 0);
+
+	print_permutations();
+
+	return 0;
+}
---
+++ Recursion/Permutation_without_overlaping.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+std::vector<std::string> permutations;
+
+//이 함수는 정렬된 문자열을 입력으로 줘야 작동한다.
+void permutation(std::string& inputstr, int current_index)
+{
+	//탈출 조건: 현재 위치가 문자열 끝인 경우
+	if (current_index == inputstr.size() - 1)
+	{
+		std::cout << inputstr << std::endl;
+		permutations.emplace_back(inputstr);
+		return;
+	}
+
+	// 현재 문자로 시작하는 경우 출력
+	permutation(inputstr, current_index + 1);
+
+	// 현재 문자로 시작하지 않는 경우 출력(각 자리 문자와 현재 문자 위치 바꿔서 출력)
+	for (int i = current_index+1; i < inputstr.size(); ++i)
+	{
+		// 현재 문자 위치 포함해서 정렬
+		std::sort(inputstr.begin()+current_index, inputstr.end());
+
+		// 현재 문자 위치와 다음 문자 위치 교환
+		char temp = inputstr[current_index];
+		inputstr[current_index] = inputstr[i];
+		inputstr[i] = temp;
+		
+		// 교환 후 현재 문자 위치 재외하고 다시 정렬하여 재귀 진입
+		std::sort(inputstr.begin() + current_index + 1, inputstr.end());
+		permutation(inputstr, current_index + 1);
+	}
+}
+/*
+int main()
+{
+	std::string inputstring= "abcd";
+
+	//같은 것이 있는 순열은 작동하지 않는다.
+	permutation(inputstring, 0);
+
+	return 0;
+}
+*/
